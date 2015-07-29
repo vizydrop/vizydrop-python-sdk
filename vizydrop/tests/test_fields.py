dfb94a5a8428c1740045b7bc361a3a6e601e8ace
Jonathan Enzinna <jonnyfunfun@gmail.com>
2015-07-29 11:50:47 -0400

    No need to test removed functionality

--- vizydrop/tests/test_fields.py
+++ vizydrop/tests/test_fields.py
@@ -23,37 +23,3 @@ class FieldTypeTests(unittest.TestCase):
         field.add(2)
         field.add(3)
         self.assertEqual(3, field.__len__())
-
-    def test_range_field_basics(self):
-        field = fields.RangeField()
-        field.set('a,b')
-        self.assertEqual(field.low(), 'a')
-        self.assertEqual(field.high(), 'b')
-        self.assertRaises(ValueError, field.set, ['a', 'b', 'c'])
-        field.set(['all', 'your base'])
-        self.assertEqual(field.low(), 'all')
-        self.assertEqual(field.high(), 'your base')
-
-    def test_date_range_field(self):
-        field = fields.DateRangeField()
-        field.set('2014-01-01,2015-01-01')
-        self.assertIsInstance(field.low(), date)
-        self.assertEqual(field.low(), date(2014, 1, 1))
-        self.assertIsInstance(field.high(), date)
-        self.assertEqual(field.high(), date(2015, 1, 1))
-
-    def test_datetime_range_field(self):
-        field = fields.DateTimeRangeField()
-        field.set('2014-01-01T12:34:56-0400,2015-01-01T01:23:45-0400')
-        self.assertIsInstance(field.low(), datetime)
-        self.assertEqual(field.low(), datetime(2014, 1, 1, 12, 34, 56, tzinfo=timezone(timedelta(-1, 72000))))
-        self.assertIsInstance(field.high(), datetime)
-        self.assertEqual(field.high(), datetime(2015, 1, 1, 1, 23, 45, tzinfo=timezone(timedelta(-1, 72000))))
-
-    def test_decimal_range_field(self):
-        field = fields.DecimalRangeField()
-        field.set('0.002,3.14159265359')
-        self.assertIsInstance(field.low(), Decimal)
-        self.assertAlmostEqual(field.low(), Decimal(0.002), 4)
-        self.assertIsInstance(field.high(), Decimal)
-        self.assertAlmostEqual(field.high(), Decimal(3.14159265359), 12)
