e3977557c4cd3d4f982cdaeb02995d844553da2d
Jonathan Enzinna <jonnyfunfun@gmail.com>
2015-10-21 12:49:54 -0400

    Add SDK endpoint for serving logos

---
+++ vizydrop/rest/logo.py
@@ -0,0 +1,23 @@
+from os import path
+
+from vizydrop.rest import VizydropAppRequestHandler
+from . import TpaHandlerMixin
+
+
+class LogoHandler(VizydropAppRequestHandler, TpaHandlerMixin):
+    def get(self):
+        # check for a local file
+        d = path.dirname(path.realpath(self.tpa.__file__))
+        fp = path.join(d, 'logo.svg')
+        if path.isfile(fp):
+            self.set_header('Content-Type', 'image/svg+xml')
+            with open(fp, 'r') as fh:
+                return self.write(fh.read())
+        # check our meta
+        meta = self.tpa.Meta
+        if hasattr(meta, 'logo'):
+            # use our logo specified in our meta
+            self.set_status(303)
+            return self.set_header('Location', meta.logo)
+        # and finally, if we have nothing...
+        self.set_status(204)
